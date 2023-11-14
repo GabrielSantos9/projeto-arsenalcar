28e832ae8928f0f8420a96597ca4ba8a4fd9569a
GabrielSantos9 <gabrielsantos9.programador@gmail.com>
2023-11-14 07:14:48 -0300

    b

--- arsenalcar.js
+++ arsenalcar.js
@@ -3,10 +3,16 @@ function sortear() {
   const numeroMin = Number(document.querySelector('#numero-min').value)
   const numeroMax = Number(document.querySelector('#numero-max').value)
   const numeroSorteado = (Math.floor(Math.random() * (numeroMax - numeroMin + 1)) + numeroMin);
-  var historico = {
-    numero: numeroSorteado,
-  }
-  console.log(historico.numero)
+  const elementResultado = document.createComment('div')
+  elementResultado.classList.add('resultado-valores')
+  elementResultado.innerText = numeroSorteado;
+
+  const elementResultadoValores = document.querySelector('resultado-valores')
+  elementResultadoValores.append('a')
+  // var historico = {
+  //   numero: numeroSorteado,
+  // }
+  // console.log('eu não sou bom com programação ' + `${historico.numero}`)
   document.querySelector('.resultado-sorteio').innerHTML = numeroSorteado
 }
 
