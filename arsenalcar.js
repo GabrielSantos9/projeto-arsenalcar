f4d1bcfb8c2716556a60f6a998ad722cc659f15b
GabrielSantos9 <gabrielsantos9.programador@gmail.com>
2023-11-29 07:14:43 -0300

    c

--- arsenalcar.js
+++ arsenalcar.js
@@ -1,19 +1,19 @@
-
 function sortear() {
-  const numeroMin = Number(document.querySelector('#numero-min').value)
-  const numeroMax = Number(document.querySelector('#numero-max').value)
-  const numeroSorteado = (Math.floor(Math.random() * (numeroMax - numeroMin + 1)) + numeroMin);
-  document.querySelector('.resultado-sorteio').innerHTML = numeroSorteado
-  
-  const elementResultado = document.createElement('div')
-  elementResultado.classList.add('resultado-valores')
+  const numeroMin = Number(document.querySelector("#numero-min").value);
+  const numeroMax = Number(document.querySelector("#numero-max").value);
+  const numeroSorteado =
+    Math.floor(Math.random() * (numeroMax - numeroMin + 1)) + numeroMin;
+  document.querySelector(".resultado-sorteio").innerHTML = numeroSorteado;
+
+  const elementResultado = document.createElement("div");
+  elementResultado.classList.add("resultado-valores");
   elementResultado.innerText = numeroSorteado;
 
-  const elementResultadoValores = document.querySelector('.resultado-valores')
-  elementResultadoValores.append(elementResultado)
-  // var historico = {
-  //   numero: numeroSorteado,
-  // }
-  // console.log('eu não sou bom com programação ' + `${historico.numero}`)
+  const elementResultadoValores = document.querySelector(".resultado-valores");
+  elementResultadoValores.append(elementResultado);
 }
 
+function historico() {
+  const botaoHistorico = document.querySelector('.numero-sorteado');
+  botaoHistorico.classList.add("historico-valores");
+}
