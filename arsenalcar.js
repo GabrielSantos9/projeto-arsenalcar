e20f6a5dcc775830575c7c85ef70114a4a595910
GabrielSantos9 <gabrielsantos9.programador@gmail.com>
2024-03-08 07:13:48 -0300

    Fazendo validação v1.5

--- arsenalcar.js
+++ arsenalcar.js
@@ -4,47 +4,44 @@ function sortear() {
   const numeroMin = Number(document.querySelector("#numero-min").value);
   const numeroMax = Number(document.querySelector("#numero-max").value);
 
-  if (numeroMin >= 0 && numeroMax >= numeroMin) {
-    let numeroSorteado;
-
-    do {
-      numeroSorteado =
-        Math.floor(Math.random() * (numeroMax - numeroMin + 1)) + numeroMin;
-    } while (numerosSorteados.includes(numeroSorteado));
-
-    numerosSorteados.push(numeroSorteado);
-
-    document.querySelector(".resultado-sorteio").innerHTML = numeroSorteado;
-
-    const elementResultado = document.createElement("div");
-    elementResultado.classList.add("resultado-valor");
-    elementResultado.innerText = numeroSorteado;
-
-    const elementResultadoValores = document.querySelector(
-      ".conteudo-historico"
-    );
-    elementResultadoValores.appendChild(elementResultado);
-    if (numerosSorteados.length === numeroMax - numeroMin + 1) {
-      reiniciarSorteio();
-      alert(
-        "Todos os números possíveis foram sorteados. O sorteio será reiniciado."
-      );
-      return;
-    }
-    if (numeroMin === 0 || numeroMax === 0) {
-      alert("Por favor, preencha os campos do sorteio.");
-    }
-  } else {
+  if (numeroMin < 0 || numeroMax < 0) {
     alert("Por favor, insira números positivos.");
+    return;
   }
-}
 
-function reiniciarSorteio() {
-  numerosSorteados = [];
-  numerosPossiveis = [];
+  if (numeroMax <= numeroMin) {
+    alert("O número máximo deve ser maior que o número mínimo.");
+    return;
+  }
 
-  document.querySelector(".resultado-sorteio").innerHTML = "";
-  document.querySelector(".conteudo-historico").innerHTML = "";
+  if (numerosSorteados.length === numeroMax - numeroMin + 1) {
+    alert(
+      "Todos os números possíveis foram sorteados. Caso queira sortear novamente, por favor, reinicie o sorteio (F5)!"
+    );
+    return;
+  }
+
+  if (numeroMin === 0 || numeroMax === 0) {
+    alert("Por favor, preencha os campos do sorteio.");
+  }
+
+  let numeroSorteado;
+
+  do {
+    numeroSorteado =
+      Math.floor(Math.random() * (numeroMax - numeroMin + 1)) + numeroMin;
+  } while (numerosSorteados.includes(numeroSorteado));
+
+  numerosSorteados.push(numeroSorteado);
+
+  document.querySelector(".resultado-sorteio").innerHTML = numeroSorteado;
+
+  const elementResultado = document.createElement("div");
+  elementResultado.classList.add("resultado-valor");
+  elementResultado.innerText = numeroSorteado;
+
+  const elementResultadoValores = document.querySelector(".conteudo-historico");
+  elementResultadoValores.appendChild(elementResultado);
 }
 
 function iniciarModal(modalID) {
