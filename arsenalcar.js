3ee3533b7a9ac86728ef9b88d1d4083164dccd11
GabrielSantos9 <gabrielsantos9.programador@gmail.com>
2024-03-12 07:17:26 -0300

    Fazendo validação e criando a função circulos

--- arsenalcar.js
+++ arsenalcar.js
@@ -10,24 +10,40 @@ function sortear() {
   const qtdNumeros = Number(document.querySelector("#qtd-numeros").value);
 
   if (numeroMin < 0 || numeroMax < 0) {
+    document.querySelector("#numero-min").disabled = false;
+    document.querySelector("#numero-max").disabled = false;
+    document.querySelector("#qtd-numeros").disabled = false;
     alert("Por favor, insira números positivos.");
     return;
   }
 
   if (numeroMax <= numeroMin) {
+    document.querySelector("#numero-min").disabled = false;
+    document.querySelector("#numero-max").disabled = false;
+    document.querySelector("#qtd-numeros").disabled = false;
     alert("O número máximo deve ser maior que o número mínimo.");
     return;
   }
 
   if (numerosSorteados.length === numeroMax - numeroMin + 1) {
+    document.querySelector("#numero-min").disabled = false;
+    document.querySelector("#numero-max").disabled = false;
+    document.querySelector("#qtd-numeros").disabled = false;
     alert(
       "Todos os números possíveis foram sorteados. Caso queira sortear novamente, por favor, reinicie o sorteio (F5)!"
     );
     return;
   }
 
-  if (numeroMin === 0 || numeroMax === 0) {
+  if (numeroMin == 0 || numeroMax == 0) {
+    document.querySelector("#numero-min").disabled = false;
+    document.querySelector("#numero-max").disabled = false;
+    document.querySelector("#qtd-numeros").disabled = false;
     alert("Por favor, preencha os campos do sorteio.");
+  }
+  
+  if (numeroMin === "" || numeroMax === "") {
+    alert("Por favor, preencha os campos vázios!")
   }
 
   let numeroSorteado;
@@ -44,6 +60,18 @@ function sortear() {
   const elementResultado = document.createElement("div");
   elementResultado.classList.add("resultado-valor");
   elementResultado.innerText = numeroSorteado;
+
+  function criarCirculos() {
+    const qtdNumeros = Number(document.querySelector("#qtd-numeros").value);
+    const circulosContainer = document.getElementById("circulos-container");
+    circulosContainer.innerHTML = ''; // Limpa o conteúdo atual
+
+    for (let i = 0; i < qtdNumeros; i++) {
+      const circulo = document.createElement("div");
+      circulo.classList.add("circulo");
+      circulosContainer.appendChild(circulo);
+    }
+  }
 
   const elementResultadoValores = document.querySelector(".conteudo-historico");
   elementResultadoValores.appendChild(elementResultado);
